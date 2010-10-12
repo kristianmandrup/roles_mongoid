8420107e5b3e387e22101d7d81abce02a371af08
yoda <yoda@tallon4.net>
2010-10-12 20:27:21 +0800

    Removed create code in valid_roles

--- lib/roles_mongoid/role.rb
+++ lib/roles_mongoid/role.rb
@@ -1,12 +1,6 @@
 module Roles::Base
   def valid_roles_are(*role_list)
     strategy_class.valid_roles = role_list.to_symbols
-    puts "Fired valid_roles_are"
-    if role_class_name
-      role_list.each do |name|
-        role_class_name.create(:name => name.to_s)
-      end
-    end
   end
 end
 
