32210b77a50e3c23e1bed49d212f605d4d66f264
Kristian Mandrup <kmandrup@gmail.com>
2010-11-23 09:20:06 +0100

    minor fix

--- spec/roles_mongoid/strategy/single/admin_flag_spec.rb
+++ spec/roles_mongoid/strategy/single/admin_flag_spec.rb
@@ -16,22 +16,3 @@ def api_name
 end
 
 load 'roles_mongoid/strategy/api_examples.rb'
-
-# require 'spec_helper'
-# use_roles_strategy :admin_flag
-# 
-# class User 
-#   include Mongoid::Document  
-#   include Roles::Mongoid 
-#   
-#   strategy :admin_flag, :default
-#   valid_roles_are :admin, :guest  
-# 
-#   field :name, :type => String  
-# end
-# 
-# describe "Roles for Mongoid: :admin_flag strategy" do
-#   load "roles_mongoid/user_setup"
-#   load "roles_generic/rspec/api"
-# end
-# 
